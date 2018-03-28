76239cb0f0d244d7a081e4425e1401aa9da94b51
lee <leedtan@gmail.com>
2018-03-28 17:52:28 -0400

    remove unused utils

--- utils.py
+++
@@ -1,6 +0,0 @@
-
-def build_forward_backward_hash(unique_values):
-    num_unique = len(unique_values)
-    fmap = dict(zip(unique_values, range(num_unique)))
-    bmap = dict(zip(range(num_unique), unique_values))
-    return fmap, bmap
